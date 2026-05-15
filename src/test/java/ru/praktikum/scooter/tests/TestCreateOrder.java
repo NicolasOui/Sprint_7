2b6508944a921070969eada6ca2e60b1f902694a
Nicolas <qellylight@gmail.com>
2026-05-15 11:04:08 +0300

    убрал дублирующие тесты в orderTrack, в TestParameterized добавил сохранение track в переменную, для удаление после теста через @After

--- src/test/java/ru/praktikum/scooter/tests/TestCreateOrder.java
+++ src/test/java/ru/praktikum/scooter/tests/TestCreateOrder.java
@@ -3,8 +3,8 @@ package ru.praktikum.scooter.tests;
 import org.junit.Test;
 import ru.praktikum.scooter.api.OrderSteps;
 import ru.praktikum.scooter.model.CreateOrder;
-
 import java.util.List;
+import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 public class TestCreateOrder extends BaseTest {
@@ -13,23 +13,18 @@ public class TestCreateOrder extends BaseTest {
 
     @Test
     @io.qameta.allure.junit4.DisplayName("Создание заказа")
-    @io.qameta.allure.Description("Тело ответа содержит track")
-    public void createOrderVerivicationReponseBody() {
+    @io.qameta.allure.Description("Тело ответа содержит track, код ответа 201")
+    public void createOrderVerificationResponseBody() {
         CreateOrder order = new CreateOrder(
                 "Narouto", "Uchina", "Konoha, 10 apt", "4", "+7 999 000 11 22",
                 5, "2026-06-01", "Dont ring the bell", List.of("BLACK")
         );
 
-        orderSteps.create(order)
+        orderTrack = orderSteps.create(order)
                 .then()
-                .body("track", notNullValue());
-
-    orderTrack = orderSteps.create(order)
-            .then()
-                .statusCode(201)
+                .statusCode(SC_CREATED)
                 .body("track", notNullValue())
-            .extract()
+                .extract()
                 .path("track");
     }
 }
-
